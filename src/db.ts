7edde152a4dbf07620e4f71bfc463d2a7bf72bc9
vishal verma <vishalverma4942@gmail.com>
2025-08-08 22:59:33 +0530

    updated

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/secondbrain";
+const MONGO_URI = process.env.MONGO_URI!;
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
